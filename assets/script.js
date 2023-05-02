0efc05a5df98105c74fdcc9d296522e3a64b7548
Allan Rivoiron <allan.rivoiron02@gmail.com>
2023-05-02 17:16:10 +0200

    Mise en place du défilement infini sur le carrousel

--- assets/script.js
+++ assets/script.js
@@ -38,9 +38,9 @@ const slider = document.querySelector("#banner");
 const dotsContainer = slider.querySelector(".dots");
 
 for (let i = 0; i < slides.length; i++) {
-  const dot = document.createElement("div");
-  dot.classList.add("dot");
-  dotsContainer.appendChild(dot);
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+	dotsContainer.appendChild(dot);
 }
 
 const dots = dotsContainer.querySelectorAll(".dot");
@@ -55,21 +55,33 @@ let currentSlide = 0;
 
 // Fonction de mise à jour de l'affichage du slider
 function updateSlider() {
-  dots.forEach(dot => dot.classList.remove("dot_selected"));
-  dots[currentSlide].classList.add("dot_selected");
+	dots.forEach(dot => dot.classList.remove("dot_selected"));
+	dots[currentSlide].classList.add("dot_selected");
 
-  bannerImg.setAttribute("src", "./assets/images/slideshow/" + slides[currentSlide].image);
-  tagLine.innerHTML = slides[currentSlide].tagLine;
+	bannerImg.setAttribute("src", "./assets/images/slideshow/" + slides[currentSlide].image);
+	tagLine.innerHTML = slides[currentSlide].tagLine;
 }
 
 // Au clic sur la flèche droite
 arrow_right.addEventListener("click", function() {
-  currentSlide++;
-  updateSlider();
+  	currentSlide++;
+
+	// Si l'index de la slide est supérieur ou égal au nombre de slides, on revient à la première slide
+	if (currentSlide >= slides.length) {
+		currentSlide = 0;
+	}
+
+	updateSlider();
 });
 
 // Au clic sur la flèche gauche
 arrow_left.addEventListener("click", function() {
-  currentSlide--;
-  updateSlider();
+	currentSlide--;
+
+	// Si l'index de la slide est inférieur à 0, on revient à la dernière slide
+	if (currentSlide < 0) {
+		currentSlide = slides.length - 1;
+	}	
+
+	updateSlider();
 });
